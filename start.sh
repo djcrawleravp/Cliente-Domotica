9f9ff593d0d012c2ed516c1f7d0edc2152a1cddf
djcrawleravp <djcrawler@gmail.com>
2023-11-17 14:10:56 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,8 @@ clear
 # Instalar Dependencias
 echo "Instalado Dependencias"
 echo ""
-apt-get update && apt-get upgrade -y > /dev/null 2>&1 || print_error "No se pudieron actualizar las dependencias"
+apt-get update -y > /dev/null 2>&1 || print_error "No se pudieron actualizar las dependencias"
+apt-get upgrade -y > /dev/null 2>&1 || print_error "No se pudieron actualizar las dependencias"
 apt-get install iptables -y > /dev/null 2>&1 || print_error "No se pudieron instalar iptables"
 apt-get install curl -y > /dev/null 2>&1 || print_error "No se pudo instalar curl"
 apt-get install git -y > /dev/null 2>&1 || print_error "No se pudo instalar git"
@@ -18,7 +19,7 @@ curl -s https://install.zerotier.com | sudo bash > /dev/null 2>&1 || print_error
 export PATH=/usr/sbin:$PATH
 
 # Iniciar Zero-Tier y Unirse a la red (Clientes)
-echo "Añadiendo servidor a la red de Clentes"
+echo "Añadiendo servidor a la red de Clientes"
 echo ""
 service zerotier-one start > /dev/null 2>&1 || print_error "No se pudo iniciar Zero-Tier"
 zerotier-cli join 9f77fc393ec5b201 > /dev/null 2>&1 || print_error "No se pudo unir a la red Zero-Tier"
@@ -48,15 +49,19 @@ rm -r /tmp/docker > /dev/null 2>&1 || print_error "No se pudo borrar el reposito
 # Descargar imágenes e Instalar Portainer
 echo "Descargando Imagenes de Docker"
 echo ""
-wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/descargar_imagenes.sh && chmod +x descargar_imagenes.sh && ./descargar_imagenes.sh > /dev/null 2>&1 || print_error "No se pudieron descargar las imágenes o instalar Portainer"
+wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/descargar_imagenes.sh > /dev/null 2>&1 || print_error "No se pudieron descargar las imágenes o instalar Portainer"
+chmod +x descargar_imagenes.sh > /dev/null 2>&1 || print_error "No se pudieron descargar las imágenes o instalar Portainer"
+./descargar_imagenes.sh > /dev/null 2>&1 || print_error "No se pudieron descargar las imágenes o instalar Portainer"
 echo "Instalando Portainer"
 echo ""
 docker run -dt -p 9000:9000 --name=Portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /home/djcrawleravp/docker/portainer:/data portainer/portainer:latest > /dev/null 2>&1 || print_error "No se pudo ejecutar el contenedor de Portainer"
 
-# Limpiar pantalla, obtener la dirección del coordinador Zigbee y remplazarla en el docker compose
+# Obtener la dirección del coordinador Zigbee y remplazarla en el docker compose
 echo "Detectando Coordinador y añadiendolo al docker compose"
 echo ""
-wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/agregar_coordinador.sh && chmod +x agregar_coordinador.sh && ./agregar_coordinador.sh > /dev/null 2>&1 || print_error "No se pudo ejecutar agregar_coordinador.sh"
+wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/agregar_coordinador.sh > /dev/null 2>&1 || print_error "No se pudo ejecutar agregar_coordinador.sh"
+chmod +x agregar_coordinador.sh > /dev/null 2>&1 || print_error "No se pudo ejecutar agregar_coordinador.sh"
+./agregar_coordinador.sh > /dev/null 2>&1 || print_error "No se pudo ejecutar agregar_coordinador.sh"
 
 echo ""
 echo "No te olvides de aceptar el permiso de join en Zero-Tier"
