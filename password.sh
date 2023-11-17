1cd2b87f3eb1aa0461b01ce2e964af9a4ac6261f
djcrawleravp <djcrawler@gmail.com>
2023-11-17 17:24:14 -0400

    Update password.sh

--- password.sh
+++ password.sh
@@ -5,7 +5,6 @@ docker_compose_file="/home/djcrawleravp/docker/portainer/custom_templates/2/dock
 if [ -n "$password" ]; then
     # Utilizamos sed para buscar y reemplazar la etiqueta "#Contraseña" con la contraseña
     sed -i "s|#Contraseña|$password|g" "$docker_compose_file"
-    echo "Contraseña agregada correctamente al archivo Docker Compose."
 else
     echo -e "\e[97;41mERROR: No se proporcionó una contraseña válida\e[0m"
 fi
