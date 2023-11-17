d0ac9c3262f90378ccc646d3c2111047fa03d8a8
djcrawleravp <djcrawler@gmail.com>
2023-11-17 13:18:24 -0400

    Create descargar_imagenes.sh

---
+++ descargar_imagenes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Descargar Imagenes Docker
+images=(
+  "ghcr.io/home-assistant/home-assistant:stable"
+  "eclipse-mosquitto:latest"
+  "koenkk/zigbee2mqtt:latest"
+  "nodered/node-red:latest"
+  "rhasspy/wyoming-whisper:latest"
+  "containrrr/watchtower:latest"
+)
+for image in "${images[@]}"; do
+  docker pull "$image"
