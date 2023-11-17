9dffc09b87f813aeb0e719d660d9b043d70bbbf0
djcrawleravp <djcrawler@gmail.com>
2023-11-17 16:07:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ print_error() {
 clear
 
 # Instalar Dependencias
-echo "Instalando Dependencias"
+echo "Instalando Dependencias:"
 apt-get update -y > /dev/null 2>&1 || print_error "No se pudieron actualizar las dependencias"
 apt-get upgrade -y > /dev/null 2>&1 || print_error "No se pudieron actualizar las dependencias"
 apt-get install iptables -y > /dev/null 2>&1 || print_error "No se pudieron instalar iptables"
@@ -19,13 +19,13 @@ export PATH=/usr/sbin:$PATH
 
 # Iniciar Zero-Tier y Unirse a la red (Clientes)
 echo ""
-echo "Añadiendo servidor a la red de Clientes"
+echo "Añadiendo servidor a la red de Clientes:"
 service zerotier-one start > /dev/null 2>&1 || print_error "No se pudo iniciar Zero-Tier"
 zerotier-cli join 9f77fc393ec5b201 > /dev/null 2>&1 || print_error "No se pudo unir a la red Zero-Tier"
 
 # Instalar Docker
 echo ""
-echo "Instalando Docker"
+echo "Instalando Docker:"
 if ! [ -x "$(command -v docker)" ]; then
   curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null 2>&1 || print_error "No se pudo descargar Docker"
   sudo sh get-docker.sh > /dev/null 2>&1 || print_error "No se pudo instalar Docker"
@@ -33,13 +33,13 @@ fi
 
 # Agregar djcrawleravp a sudoers y dar permiso para usar docker
 echo ""
-echo "Actualizando Permisos de usuario"
+echo "Actualizando Permisos de usuario:"
 echo "djcrawleravp ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/djcrawleravp > /dev/null 2>&1 || print_error "No se pudo agregar a djcrawleravp a sudoers"
 sudo usermod -aG docker djcrawleravp > /dev/null 2>&1 || print_error "No se pudo dar permisos de docker a djcrawleravp"
 
 # Copiar la carpeta Portainer pre configurada al servidor
 echo ""
-echo "Copiando carpetas pre configuradas"
+echo "Copiando carpetas pre configuradas:"
 chmod -R 777 /home/djcrawleravp > /dev/null 2>&1 || print_error "No se pudo cambiar permisos de la carpeta"
 git clone https://github.com/djcrawleravp/Cliente-Domotica.git /tmp/docker > /dev/null 2>&1 || print_error "No se pudo clonar el repositorio"
 mv /tmp/docker/docker /home/djcrawleravp/docker > /dev/null 2>&1 || print_error "No se pudo mover la carpeta docker"
@@ -47,7 +47,7 @@ rm -r /tmp/docker > /dev/null 2>&1 || print_error "No se pudo borrar el reposito
 
 # Descargar imágenes
 echo ""
-echo "Descargando Imágenes de Docker"
+echo "Descargando Imágenes de Docker:"
 if ! { wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/descargar_imagenes.sh && chmod +x descargar_imagenes.sh; } > /dev/null 2>&1; then
   print_error "No se pudieron descargar las imágenes o instalar Portainer"
 fi
@@ -55,12 +55,12 @@ fi
 
 # Instalar Portainer
 echo ""
-echo "Instalando Portainer"
+echo "Instalando Portainer:"
 docker run -dt -p 9000:9000 --name=Portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /home/djcrawleravp/docker/portainer:/data portainer/portainer:latest || print_error "No se pudo ejecutar el contenedor de Portainer"
 
 # Obtener la dirección del coordinador Zigbee y remplazarla en el docker compose
 echo ""
-echo "Detectando Coordinador y añadiendolo al docker compose"
+echo "Detectando Coordinador y añadiendolo al docker compose:"
 if ! { wget https://raw.githubusercontent.com/djcrawleravp/Cliente-Domotica/main/agregar_coordinador.sh && chmod +x agregar_coordinador.sh; } > /dev/null 2>&1; then
   print_error "No se pudo descargar el script agregar_coordinador.sh"
 fi
