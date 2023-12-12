746858f39e10169b6d1f9a7b33a8e15e59601647
djcrawleravp <djcrawler@gmail.com>
2023-12-12 17:24:16 -0400

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-docker_compose_file="/home/djcrawleravp/docker/portainer/custom_templates/2/docker-compose.yml"
+# Rutas de Docker Compose
+docker_compose_file_1="/home/$username/docker/portainer/custom_templates/1/docker-compose.yml"
+docker_compose_file_2="/home/$username/docker/portainer/custom_templates/2/docker-compose.yml"
 
-if [ -n "$password" ]; then
-    # Utilizamos sed para buscar y reemplazar la etiqueta "#Contraseña" con la contraseña
-    sed -i "s|#Contraseña|$password|g" "$docker_compose_file"
-else
-    echo -e "\e[97;41mERROR: No se proporcionó una contraseña válida\e[0m"
-fi
-if [ -n "$cliente" ]; then
-    # Utilizamos sed para buscar y reemplazar la etiqueta "#Cliente" con el nombre
-    sed -i "s|#Cliente|$cliente|g" "$docker_compose_file"
-else
-    echo -e "\e[97;41mERROR: No se proporcionó un nombre válido\e[0m"
-fi
-if [ -n "$username" ]; then
-    sed -i "s|#username|$username|g" "$docker_compose_file"
-else
-    echo -e "\e[97;41mERROR: No se proporcionó un nombre de usuario válido\e[0m"
-fi
+# Función para reemplazar etiquetas en un archivo docker-compose.yml
+replace_labels() {
+    local file="$1"
+    if [ -n "$password" ]; then
+        sed -i "s|#Contraseña|$password|g" "$file"
+    else
+        echo -e "\e[97;41mERROR: No se proporcionó una contraseña válida\e[0m"
+    fi
+
+    if [ -n "$cliente" ]; then
+        sed -i "s|#Cliente|$cliente|g" "$file"
+    else
+        echo -e "\e[97;41mERROR: No se proporcionó un nombre válido\e[0m"
+    }
+
+    if [ -n "$username" ]; then
+        sed -i "s|#username|$username|g" "$file"
+    else
+        echo -e "\e[97;41mERROR: No se proporcionó un nombre de usuario válido\e[0m"
+    fi
+}
+
+# Reemplazar etiquetas en el primer archivo
+replace_labels "$docker_compose_file_1"
+
+# Reemplazar etiquetas en el segundo archivo
+replace_labels "$docker_compose_file_2"
